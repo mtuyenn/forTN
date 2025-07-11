a5ac22d8b0d3011ebc0713bfe7ad32d1a03df911
mtuyenn <minhtuyen220706@gmail.com>
2025-07-11 21:31:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const yesBtn = document.querySelector('.btn--yes');
 const noBtn = document.querySelector('.btn--no');
 
 yesBtn.addEventListener('click', () => {
-  window.location.href = "thankyou.html";
+  window.location.href = "thankyou.html"; 
 });
 
 noBtn.addEventListener('mouseover', () => {
-  const x = Math.floor(Math.random() * window.innerWidth - 100);
-  const y = Math.floor(Math.random() * window.innerHeight - 50);
+  const x = Math.floor(Math.random() * (window.innerWidth - 100));
+  const y = Math.floor(Math.random() * (window.innerHeight - 50));
   noBtn.style.position = 'absolute';
   noBtn.style.left = `${x}px`;
   noBtn.style.top = `${y}px`;
